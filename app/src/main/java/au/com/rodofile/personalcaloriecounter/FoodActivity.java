2452e592d81b1e9787271cb3fc5dc65afc734e20
nrodofile <nrodofile@gmail.com>
2016-10-20 01:15:26 +1000

    Made view for food

--- app/src/main/java/au/com/rodofile/personalcaloriecounter/FoodActivity.java
+++ app/src/main/java/au/com/rodofile/personalcaloriecounter/FoodActivity.java
@@ -2,12 +2,22 @@ package au.com.rodofile.personalcaloriecounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class FoodActivity extends AppCompatActivity {
+    private String[] measurementSpinner;
     Food food = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
+        this.measurementSpinner = new String[] {
+                "grams", "millilitres"
+        };
+        Spinner s = (Spinner) findViewById(R.id.spinnerMeasurement);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, measurementSpinner);
+        s.setAdapter(adapter);
     }
 }
